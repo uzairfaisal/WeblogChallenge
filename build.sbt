55ca7ab7f39d321fd0a3e5fd219cfcbf42023436
Uzair Faisal <uzairfaisal786@gmail.com>
2016-09-12 17:45:09 -0400

    add case class for weblog entry and test code for reading the weblog file

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "WeblogChallenge"
 version := "1.0"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.0.0"
+)
--- src/main/scala/WeblogChallenge.scala
+++ src/main/scala/WeblogChallenge.scala
@@ -1,3 +1,47 @@
+package com.paytm
+
+import java.time.LocalDateTime
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+case class IPPort(ip: String, port: Int)
+
+case class Weblog(
+  timestamp: LocalDateTime,
+  elb: String,
+  clientIPPort: IPPort,
+  backendIPPort: Option[IPPort],
+  requestProcessingTime: Int,
+  backendProcessingTime: Int,
+  responseProcessingTime: Int,
+  elbStatusCode: String,
+  backendStatusCode: String,
+  receivedBytes: Int,
+  sentBytes: Int,
+  request: String,
+  userAgent: String,
+  sslCipher: String,
+  sslProtocol: String
+)
+
 object WeblogChallenge {
-  def main(args: Array[String]) = println("Hi!")
+  val AppName = "PaytmWeblogChallenge"
+  val LocalMode = "local"
+
+  def main(args: Array[String]) = {
+    val logPath: String = inputFromArgs(args)
+    val context = new SparkContext(new SparkConf().setAppName(AppName).setMaster(LocalMode))
+    sessionize(context, logPath)
+    context.stop()
+  }
+
+  def inputFromArgs(args: Array[String]): String = {
+    if (args.length < 1) throw new IllegalArgumentException("Path to weblog file is missing")
+    args(0)
+  }
+
+  def sessionize(context: SparkContext, dataPath: String) {
+    println(context.textFile(dataPath)
+      .repartition(16).count())
+  }
 }
